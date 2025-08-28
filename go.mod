dd905ef1244c7a666fc605cf7a1ab08a2ff8cd79
ggualbertosouza <contatoguilhermegsz@gmail.com>
2025-08-27 22:08:19 -0300

    Init: Rate limiting project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ggualbertosouza/rate-limiter
+
+go 1.24.3
---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Iniciando o projeto aqui oo, vai dar certo")
+}
